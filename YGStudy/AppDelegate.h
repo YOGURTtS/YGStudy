1be4cc9098b53ea511fd32f83654f5e898a56146
YOGURTS <scofield838727836@vip.qq.com>
2019-09-02 21:03:58 +0800

    project initial

---
+++ YGStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YGStudy
+//
+//  Created by 孙星 on 2019/9/2.
+//  Copyright © 2019 YOGURTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YGStudy/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YGStudy
+//
+//  Created by 孙星 on 2019/9/2.
+//  Copyright © 2019 YOGURTS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
