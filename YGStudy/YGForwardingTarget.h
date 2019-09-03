b8c8459e8363d56948e97e4afa07963dbde98dc2
YOGURTS <scofield838727836@vip.qq.com>
2019-09-03 22:06:14 +0800

    睡觉

---
+++ YGStudy/YGForwardingTarget.h
@@ -0,0 +1,19 @@
+//
+//  YGForwardingTarget.h
+//  YGStudy
+//
+//  Created by 孙星 on 2019/9/3.
+//  Copyright © 2019 YOGURTS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YGForwardingTarget : NSObject
+
+- (void)haha;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- YGStudy/YGForwardingTarget.m
+++ YGStudy/YGForwardingTarget.m
@@ -1,46 +1,41 @@
 //
-//  YGMessageForwardObject.m
+//  YGForwardingTarget.m
 //  YGStudy
 //
-//  Created by 孙星 on 2019/9/2.
+//  Created by 孙星 on 2019/9/3.
 //  Copyright © 2019 YOGURTS. All rights reserved.
 //
 
-#import "YGMessageForwardObject.h"
 #import "YGForwardingTarget.h"
 #import <objc/runtime.h>
 
-@implementation YGMessageForwardObject
+@implementation YGForwardingTarget
 
-void hehe() {
-    NSLog(@"呵呵");
+- (void)haha {
+    NSLog(@"哈哈");
 }
 
-#pragma mark - 动态方法决议
-/**
- Objective C 提供了一种名为动态方法决议的手段，使得我们可以在运行时动态地为一个 selector 提供实现。
- 我们只要实现 +resolveInstanceMethod: 或 +resolveClassMethod: 方法，
- 并在其中为指定的 selector 提供实现即可（通过调用运行时函数 class_addMethod 来添加）。
- */
-
+void heihei() {
+    NSLog(@"YGForwardingTarget.h -- 呵呵");
+}
 
 /** 解决类方法 */
 + (BOOL)resolveClassMethod:(SEL)sel {
     NSLog(@"+ (BOOL)resolveClassMethod:(SEL)sel");
-//    if (sel == @selector(haha)) {
-//        class_addMethod(self, sel, (IMP)hehe, "v");
-//        return NO;
-//    }
+    if (sel == @selector(haha)) {
+        class_addMethod(self, sel, (IMP)heihei, "v");
+        return NO;
+    }
     return [super resolveClassMethod:sel];
 }
 
 /** 解决实例方法 */
 + (BOOL)resolveInstanceMethod:(SEL)sel {
     NSLog(@"- (BOOL)resolveClassMethod:(SEL)sel");
-//    if (sel == @selector(haha)) {
-//        class_addMethod(self, sel, (IMP)hehe, "v");
-//        return NO;
-//    }
+    if (sel == @selector(haha)) {
+        class_addMethod(self, sel, (IMP)heihei, "v");
+        return NO;
+    }
     return [super resolveClassMethod:sel];
 }
 
@@ -48,17 +43,11 @@ void hehe() {
 
 + (id)forwardingTargetForSelector:(SEL)aSelector {
     NSLog(@"+ (id)forwardingTargetForSelector:(SEL)aSelector");
-//    if (aSelector == @selector(haha)) {
-//        return [YGForwardingTarget class];
-//    }
     return [super forwardingTargetForSelector:aSelector];
 }
 
 - (id)forwardingTargetForSelector:(SEL)aSelector {
     NSLog(@"- (id)forwardingTargetForSelector:(SEL)aSelector");
-//    if (aSelector == @selector(haha)) {
-//        return [YGForwardingTarget new];
-//    }
     return [super forwardingTargetForSelector:aSelector];
 }
 
@@ -66,15 +55,11 @@ void hehe() {
 
 + (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
     NSLog(@"+ (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector");
-    return [YGForwardingTarget methodSignatureForSelector:aSelector];
     return [super methodSignatureForSelector:aSelector];
 }
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
     NSLog(@"- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector");
-    if (aSelector == @selector(haha)) {
-        return [YGForwardingTarget instanceMethodSignatureForSelector:aSelector];
-    }
     return [super methodSignatureForSelector:aSelector];
 }
 
@@ -82,20 +67,11 @@ void hehe() {
 
 + (void)forwardInvocation:(NSInvocation *)anInvocation {
     NSLog(@"+ (void)forwardInvocation:(NSInvocation *)anInvocation");
-    if ([YGForwardingTarget respondsToSelector:[anInvocation selector]]) {
-        [anInvocation invokeWithTarget:[YGForwardingTarget class]];
-        return;
-    }
     [super forwardInvocation:anInvocation];
 }
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
     NSLog(@"- (void)forwardInvocation:(NSInvocation *)anInvocation");
-    NSLog(@"[anInvocation selector] = %@", NSStringFromSelector([anInvocation selector] ));
-    if ([[YGForwardingTarget new] respondsToSelector:[anInvocation selector]]) {
-        [anInvocation invokeWithTarget:[YGForwardingTarget new]];
-        return;
-    }
     [super forwardInvocation:anInvocation];
 }
 
@@ -110,6 +86,5 @@ void hehe() {
     NSLog(@"- (void)doesNotRecognizeSelector:(SEL)aSelector");
     [super doesNotRecognizeSelector:aSelector];
 }
-
 
 @end
